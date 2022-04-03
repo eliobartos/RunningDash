908b46249e3691d008a895b999009b8f4a494420
Andrijana Brkic <andrijana@lebesgue.io>
2022-04-03 21:42:41 +0200

    added cards in individual tab

--- server/func_individual.R
+++ server/func_individual.R
@@ -1,7 +1,7 @@
 graph_distance_vs_run = function(data_runner) {
   data_runner_sorted = data_runner %>% arrange(date) %>% mutate(run = 1:nrow(data_runner))
 
-  plot_ly(data = data_runner_sorted, x = ~run, y = ~distance, color = 'rgb(129, 222, 199)',
+  plot_ly(data = data_runner_sorted, x = ~run, y = ~distance, marker = list(color = 'rgb(158,202,225)'),
           type = "bar") %>%
     layout(
       xaxis = list(rangemode = "tozero", zeroline = FALSE),
@@ -11,7 +11,7 @@ graph_distance_vs_run = function(data_runner) {
 graph_pace_vs_run = function(data_runner) {
   data_runner_sorted = data_runner %>% arrange(date) %>% mutate(run = 1:nrow(data_runner))
 
-  plot_ly(data = data_runner_sorted, x = ~run, y = ~pace, color = 'rgb(129, 222, 199)',
+  plot_ly(data = data_runner_sorted, x = ~run, y = ~pace, marker = list(color = 'rgb(158,202,225)'),
           type = "bar") %>%
     layout(
       xaxis = list(rangemode = "tozero", zeroline = FALSE),
@@ -20,9 +20,22 @@ graph_pace_vs_run = function(data_runner) {
 
 
 graph_distance_vs_date = function(data_runner) {
-  plot_ly(data = data_runner, x = ~date, y = ~distance, color = 'rgb(129, 222, 199)',
+  plot_ly(data = data_runner, x = ~date, y = ~distance, marker = list(color = 'rgb(158,202,225)'),
           type = "bar") %>%
     layout(
       xaxis = list(rangemode = "tozero", zeroline = FALSE),
       yaxis = list(rangemode = "tozero", zeroline = FALSE))
+}
+
+individual_boxes = function(data_runner) {
+  total_runs = data_runner %>% nrow()
+  total_distance = sum(data_runner$distance)
+  total_time = sum(data_runner$time_s)
+  avg_pace = (total_time/total_distance)
+
+  return(list(
+    total_runs = total_runs %>% as.character() ,
+    total_distance = total_distance %>% as.character(),
+    total_time = total_time %>% seconds_to_hms(),
+    avg_pace = round(avg_pace,2) %>% as.character()))
 }
--- ui/ui_individual.R
+++ ui/ui_individual.R
@@ -1,7 +1,14 @@
 tab_individual = tabItem(
     tabName = "individual",
     uiOutput("runner_filter"),
-    
+    br(),
+    fluidRow(
+      valueBoxOutput("total_runs_ind", width = 3),
+      valueBoxOutput("total_distance_ind", width = 3),
+      valueBoxOutput("total_time_ind", width = 3),
+      valueBoxOutput("avg_pace_ind", width = 3)
+    ),
+
     h2("Runs over time"),
     br(),
     fluidRow(
@@ -18,14 +25,14 @@ tab_individual = tabItem(
         title = "Distance vs Run",
         status = "primary",
         solidHeader = TRUE,
-        plotlyOutput("distance_vs_run")  
+        plotlyOutput("distance_vs_run")
        ),
        box(
          title = "Pace vs Run",
          status = "primary",
          solidHeader = TRUE,
-         plotlyOutput("pace_vs_run")  
+         plotlyOutput("pace_vs_run")
       )
     )
-                         
+
 )
