73c0465c5e1d6b763a6ba2da9a6f174e15a4d82f
Andrijana Brkic <andrijana@lebesgue.io>
2022-04-03 12:21:39 +0200

    packages to init file

---
+++ init.R
@@ -0,0 +1,26 @@
+packages = c(
+    # Shiny Related Packages
+    "shiny",
+    "shinydashboard",
+    "shinyWidgets",
+    "shinyTime",
+
+    # DB Related Packages
+    "DBI",
+    "RPostgreSQL",
+
+    # Other Packages
+    "plotly",
+    "lubridate",
+    "dplyr",
+    "magrittr"
+    )
+
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+
+invisible(sapply(packages, install_if_missing))
+invisible(lapply(packages, library, character.only = TRUE))
