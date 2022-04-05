b00c2ab550ad41c207f2efba6971bab075706cef
Elio Bartos <eliobartos@yahoo.com>
2022-04-05 21:39:41 +0200

    re-created graphs on overall tab with ggplot2 and plotly. Added axis limits, tooltips...

--- server/func_overall.R
+++ server/func_overall.R
@@ -2,15 +2,48 @@
 # Graphs ------------------------------------------------------------------
 graph_pace_vs_distance = function(data) {
   
-  plot_ly(data = data, x = ~distance, y = ~pace, color =~runner,
-          marker = list(size = 10)) %>% 
-    layout(
-      xaxis = list(rangemode = "tozero", zeroline = FALSE), 
-      yaxis = list(rangemode = "tozero", zeroline = FALSE))
+  hover_data = list()
+  for(i in 1:nrow(data)) {
+      hover_data[[i]] = paste0(
+        "<b>Runner:</b>   ", data$runner[i], "\n",
+        "<b>Distance:</b> ", data$distance[i], " km", "\n",
+        "<b>Pace:</b>       ", data$pace_pretty[i], "\n",
+        "<b>Date:</b>        ", data$date[i]
+      )
+  }
+  
+  data$hover_data = hover_data
+  
+  p = data %>% 
+    ggplot(aes(x = distance, y = pace, color = runner)) +
+    geom_point() +
+    xlab("Distance (km)") +
+    ylab("Pace (min/km)") +
+    expand_limits(y = 4, x = 1) +
+    scale_x_continuous(breaks = seq(0, 10), minor_breaks = NULL) +
+    scale_y_continuous(n.breaks = 6, minor_breaks = NULL) +
+    labs(color = "Runner") +
+    theme_minimal() 
+  
+  fig = plotly_build(p)
+  
+  
+  for(i in 1:length(unique(data$runner))) {
+    tmp = data %>% filter(runner == fig$x$data[[i]]$name)
+    fig$x$data[[i]]$text = tmp$hover_data
+  }
+
+  return(fig)
+  
 }
 
 
+
+
+
+
 graph_avg_pace = function(data) {
+  
   tmp = data %>% 
     mutate(
       distance_group = cut(distance, breaks = c(0, seq(0.5, 100.5, by = 1), Inf), labels = c(0, seq(1, 100, by = 1), 101)) %>% as.character() %>%  as.numeric()
@@ -18,17 +51,50 @@ graph_avg_pace = function(data) {
     group_by(runner, distance_group) %>% 
     summarise(
       time = sum(time_s),
+      runs = n(),
       distance_ = sum(distance),
+      avg_pace_s = sum(time_s)/sum(distance),
       avg_pace = sum(time_s)/sum(distance)/60,
-      avg_pace_pretty = minutes_to_ms(avg_pace)
+      avg_pace_pretty = seconds_to_hms(avg_pace_s)
     )
   
-  plot_ly(data = tmp, x = ~distance_group, y =~ avg_pace, color =~runner, type = 'scatter',
-          marker = list(size = 10)) %>% 
-    layout(
-      xaxis = list(rangemode = "tozero", zeroline = FALSE), 
-      yaxis = list(rangemode = "tozero", zeroline = FALSE)
+  hover_data = list()
+  for(i in 1:nrow(tmp)) {
+    hover_data[[i]] = paste0(
+      "<b>Runner:    </b>", tmp$runner[i], "\n",
+      "<b>Runs:        </b>", tmp$runs[i], "\n",
+      "<b>Distance:  </b>", tmp$distance_group[i], " km", "\n",
+      "<b>Avg Pace: </b>", tmp$avg_pace_pretty[i]
     )
+  }
+  
+  tmp$hover_data = hover_data
+  
+  p = tmp %>% 
+    ggplot(aes(x = distance_group, y = avg_pace, color = runner)) +
+    geom_point(alpha = 0.8) +
+    xlab("Distance Group") +
+    ylab("Avg. Pace") +
+    expand_limits(y = 4, x = 1) +
+    scale_x_continuous(breaks = seq(0, 10), minor_breaks = NULL) +
+    scale_y_continuous(n.breaks = 6, minor_breaks = NULL) +
+    labs(color = "Runner") +
+    theme_minimal() 
+  
+  
+  p2 <- ggplotly(p)
+  
+  p2 <- p2 %>% layout(hoverlabel = list(align = "left"))
+  
+  fig = plotly_build(p2)
+  
+  for(i in 1:length(unique(tmp$runner))) {
+    tmp2 = tmp %>% filter(runner == fig$x$data[[i]]$name)
+    fig$x$data[[i]]$text = tmp2$hover_data
+  }
+  
+  return(fig)
+  
 }
 
 
