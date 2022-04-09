f9e31d68883f3d2d1ae1468b19319665669f3e9c
Andrijana Brkic <andrijana@lebesgue.io>
2022-04-09 15:10:46 +0200

    individual graphs pretty version

--- server/func_individual.R
+++ server/func_individual.R
@@ -1,45 +1,109 @@
-graph_distance_vs_run = function(data_runner) {
+graph_distance_vs_run = function(data_runner, min_date) {
 
   data_runner_sorted = data_runner %>% arrange(date) %>% mutate(run = 1:nrow(data_runner))
+  max_run = max(data_runner_sorted$run) + 1
 
-  plot_ly(data = data_runner_sorted,
-    x = ~run,
-    y = ~distance,
-    marker = list(color = 'rgb(158,202,225)'),
-    type = "bar"
-  ) %>% layout(
-    xaxis = list(rangemode = "tozero", zeroline = FALSE),
-    yaxis = list(rangemode = "tozero", zeroline = FALSE)
-  )
+  hover_data = list()
+  for(i in 1:nrow(data_runner_sorted)) {
+      hover_data[[i]] = paste0(
+        "<b>Distance:</b> ", data_runner_sorted$distance[i], " km", "\n",
+        "<b>Time:</b>       ", data_runner_sorted$time_s[i] %>% seconds_to_hms(), "\n",
+        "<b>Pace:</b>       ", data_runner_sorted$pace_pretty[i], "\n",
+        "<b>Date:</b>        ", data_runner_sorted$date[i]
+      )
+  }
+
+  p = data_runner_sorted %>%
+    ggplot(aes(x = run, y = distance)) +
+    geom_bar(stat="identity", fill="lightblue", width = 0.9) +
+    xlab("Run #") +
+    ylab("Distance (km)") +
+    scale_x_continuous(
+      limits = c(0, max_run),
+      breaks = data_runner_sorted$run
+      ) +
+    theme_minimal()
+
+  p2 = ggplotly(p) %>% layout(hoverlabel = list(align = "left"))
+
+  fig = plotly_build(p2)
+
+  fig$x$data[[1]]$text = hover_data
+
+  return(fig)
 }
 
-graph_pace_vs_run = function(data_runner) {
+graph_pace_vs_run = function(data_runner, min_date) {
 
   data_runner_sorted = data_runner %>% arrange(date) %>% mutate(run = 1:nrow(data_runner))
+  max_run = max(data_runner_sorted$run) + 1
 
-  plot_ly(data = data_runner_sorted,
-    x = ~run,
-    y = ~pace,
-    marker = list(color = 'rgb(158,202,225)'),
-    type = "bar"
-  ) %>% layout(
-    xaxis = list(rangemode = "tozero", zeroline = FALSE),
-    yaxis = list(rangemode = "tozero", zeroline = FALSE)
-  )
+  hover_data = list()
+  for(i in 1:nrow(data_runner_sorted)) {
+      hover_data[[i]] = paste0(
+        "<b>Distance:</b> ", data_runner_sorted$distance[i], " km", "\n",
+        "<b>Time:</b>       ", data_runner_sorted$time_s[i] %>% seconds_to_hms(), "\n",
+        "<b>Pace:</b>       ", data_runner_sorted$pace_pretty[i], "\n",
+        "<b>Date:</b>        ", data_runner_sorted$date[i]
+      )
+  }
+
+  p = data_runner_sorted %>%
+    ggplot(aes(x = run, y = pace, group = 1)) +
+    geom_point(size = 4, color ="orange") +
+    geom_line(size = 1, color ="orange") +
+    xlab("Run #") +
+    ylab("Pace (min/km)") +
+    scale_x_continuous(
+      limits = c(0, max_run),
+      breaks = data_runner_sorted$run
+      ) +
+    expand_limits(y = 4) +
+    theme_minimal()
+
+  p2 = ggplotly(p) %>% layout(hoverlabel = list(align = "left"))
+
+  fig = plotly_build(p2)
+
+  fig$x$data[[1]]$text = hover_data
+
+  return(fig)
 }
 
 
-graph_distance_vs_date = function(data_runner) {
+graph_distance_vs_date = function(data_runner, min_date) {
 
-  plot_ly(data = data_runner,
-   x = ~date,
-   y = ~distance,
-   marker = list(color = 'rgb(158,202,225)'),
-   type = "bar"
-  ) %>% layout(
-    xaxis = list(rangemode = "tozero", zeroline = FALSE),
-    yaxis = list(rangemode = "tozero", zeroline = FALSE)
-  )
+  hover_data = list()
+  for(i in 1:nrow(data_runner)) {
+      hover_data[[i]] = paste0(
+        "<b>Distance:</b> ", data_runner$distance[i], " km", "\n",
+        "<b>Pace:</b>       ", data_runner$pace_pretty[i], "\n",
+        "<b>Date:</b>        ", data_runner$date[i]
+      )
+  }
+
+  data_runner$hover_data = hover_data
+
+  p = data_runner %>%
+    ggplot(aes(x = date, y = distance)) +
+    geom_bar(stat="identity", fill="lightblue", width = 0.9) +
+    xlab("Date") +
+    ylab("Distance (km)") +
+    scale_x_date(
+      date_breaks = "2 days",
+      limits = c(min_date-1, today() + 1),
+      date_labels = "%b %d"
+      ) +
+    theme_minimal()
+
+  p2 = ggplotly(p) %>% layout(hoverlabel = list(align = "left"))
+
+  fig = plotly_build(p2)
+
+  fig$x$data[[1]]$text = hover_data
+
+  return(fig)
+
 }
 
 get_individual_stats = function(data_runner) {
--- server/server.R
+++ server/server.R
@@ -8,14 +8,16 @@ server = function(input, output) {
     filter_data(data_all, input$date_filter)
   })
 
-  min_date = get_min_date(min_date_all, input$date_filter)
+  min_date = reactive({
+    get_min_date(min_date_all, input$date_filter)
+  })
 
   output$runner_filter = renderUI({
     runners =  data()$runner %>% unique()
     selectInput("runner_filter",
                 label = "Runner:",
                 choices = runners,
-                selected = runners[1] )
+                selected = runners[1])
   })
 
   data_runner = reactive({
@@ -36,7 +38,7 @@ server = function(input, output) {
   })
 
   output$third_runner = renderValueBox({
-    valueBox(boxes_runner()$runner[3], boxes_runner()$final_string[3], icon = icon("listfaet"), color = "blue")
+    valueBox(boxes_runner()$runner[3], boxes_runner()$final_string[3], icon = icon("play"), color = "blue")
   })
 
   output$pace_vs_distance = renderPlotly({ graph_pace_vs_distance(data()) })
@@ -52,26 +54,26 @@ server = function(input, output) {
     })
 
   output$total_runs_ind = renderValueBox({
-    valueBox(boxes()["total_runs"], "Total runs", icon = icon("hashtag"), color = "orange")
+    valueBox(boxes()["total_runs"], "Total runs", icon = icon("hashtag"), color = "yellow")
   })
 
   output$total_distance_ind = renderValueBox({
-    valueBox(boxes()["total_distance"], "Total distance", icon = icon("play"), color = "green")
+    valueBox(boxes()["total_distance"], "Total distance", icon = icon("play"), color = "blue")
   })
 
   output$total_time_ind = renderValueBox({
-    valueBox(boxes()["total_time"], "Total running time", icon = icon("clock"), color = "yellow")
+    valueBox(boxes()["total_time"], "Total running time", icon = icon("clock"), color = "green")
   })
 
   output$avg_pace_ind = renderValueBox({
-    valueBox(boxes()["avg_pace"], "Average pace", icon = icon("redo"), color = "blue")
+    valueBox(boxes()["avg_pace"], "Average pace", icon = icon("redo"), color = "orange")
   })
 
-  output$distance_vs_run = renderPlotly({ graph_distance_vs_run(data_runner()) })
+  output$distance_vs_run = renderPlotly({ graph_distance_vs_run(data_runner(), min_date()) })
 
-  output$pace_vs_run = renderPlotly({ graph_pace_vs_run(data_runner()) })
+  output$pace_vs_run = renderPlotly({ graph_pace_vs_run(data_runner(), min_date()) })
 
-  output$distance_vs_date = renderPlotly({ graph_distance_vs_date(data_runner()) })
+  output$distance_vs_date = renderPlotly({ graph_distance_vs_date(data_runner(), min_date()) })
 
 
   # Input -------------------------------------------------------------------
